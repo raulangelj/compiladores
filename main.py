8e7968d4b876dbd291b71f9964c9ebb5c8514beb
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-07-16 00:37:57 -0600

    ref: organice the main file

--- main.py
+++ main.py
@@ -8,53 +8,32 @@ from dist.grammar.yaplListener import yaplListener
 from antlr4.tree.Trees import Trees
 
 def main():
-    # if len(sys.argv) < 2:
-    #     print("ERROR: A FILE *.yapl must be pass.\nUsage: main.py <input file>")
-    #     exit(0)
+    if len(sys.argv) < 2:
+        raise Exception("ERROR: A FILE *.yapl must be pass.\nUsage: main.py <input file>")
+
     # Load grammar
     try:
-        # file = sys.argv[1]
-        file = '.\\tests\simple.yapl'
+        file = sys.argv[1]
         input_stream = FileStream(file)
-        
-        # lexer
-        lexer = yaplLexer(input_stream)
-        token_stream = CommonTokenStream(lexer)
-
-        # parser
-        parser = yaplParser(token_stream)
-        tree = parser.program()
-
-        # display parse tree in text form
-        print(Trees.toStringTree(tree, None, parser))
-
-        # display parse tree in GUI
-        command = f'antlr4-parse .\\grammar\\yapl.g4 program -gui'
-        process = os.popen(command, 'w')
-        process.write(input_stream.strdata)
-        process.close()
-
-        listener = yaplListener()
-        walker = ParseTreeWalker()
-        walker.walk(listener, tree)
     except Exception as e:
-        raise Exception(f"ERROR: The file {file} does not exist.", e)
-    # # Create an input stream from the expression
-    # expression = "2*(3+4)"
-    # input_stream = InputStream(expression)
+        raise Exception(f"ERROR: openong the file {file}") from e
 
-    # # Create a lexer and token stream
-    # lexer = ExprLexer(input_stream)
-    # token_stream = CommonTokenStream(lexer)
+    # lexer
+    lexer = yaplLexer(input_stream)
+    token_stream = CommonTokenStream(lexer)
 
-    # # Create a parser and specify the token stream
-    # parser = ExprParser(token_stream)
+    # parser
+    parser = yaplParser(token_stream)
+    tree = parser.program()
 
-    # # Parse the expression and get the parse tree
-    # parse_tree = parser.expr()
+    # display parse tree in text form
+    print(Trees.toStringTree(tree, None, parser))
 
-    # # Print the parse tree
-    # print(parse_tree.toStringTree(recog=parser))
+    # display parse tree in GUI
+    command = f'antlr4-parse .\\grammar\\yapl.g4 program -gui'
+    process = os.popen(command, 'w')
+    process.write(input_stream.strdata)
+    process.close()
 
 if __name__ == '__main__':
     main()
